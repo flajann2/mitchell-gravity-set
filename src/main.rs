23d06860af35211b591b7574898dfefc4f443989
Fred Mitchell <fred.mitchell@gmx.de>
2017-07-09 17:53:06 +0200

    piston was a bit of a trick, but now works

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,83 @@ mod feature {
         let font_path = assets.join("fonts/NotoSans/NotoSans-Regular.ttf");
         ui.fonts.insert_from_file(font_path).unwrap();
 
+        // Create a texture to use for efficiently caching text on the GPU.
+        let mut text_vertex_data = Vec::new();
+        let (mut glyph_cache, mut text_texture_cache) = {
+            const SCALE_TOLERANCE: f32 = 0.1;
+            const POSITION_TOLERANCE: f32 = 0.1;
+            let cache = conrod::text::GlyphCache::new(WIDTH, HEIGHT, SCALE_TOLERANCE, POSITION_TOLERANCE);
+            let buffer_len = WIDTH as usize * HEIGHT as usize;
+            let init = vec![128; buffer_len];
+            let settings = TextureSettings::new();
+            let factory = &mut window.factory;
+            let texture = G2dTexture::from_memory_alpha(factory, &init, WIDTH, HEIGHT, &settings).unwrap();
+            (cache, texture)
+        };
+
+        let ids = support::Ids::new(ui.widget_id_generator());
+
+        let mgs_logo: G2dTexture = {
+            let assets = find_folder::Search::ParentsThenKids(3, 3).for_folder("assets").unwrap();
+            let path = assets.join("images/mgs3.png");
+            let factory = &mut window.factory;
+            let settings = TextureSettings::new();
+            Texture::from_path(factory, &path, Flip::None, &settings).unwrap()
+        };
+
+        let mut image_map = conrod::image::Map::new();
+        let mgs_logo = image_map.insert(mgs_logo);
+
+        let mut app = support::MGSApp::new(mgs_logo);
+
         while let Some(event) = window.next(){
+            // Convert the piston event to a conrod event.
+            let size = window.size();
+            let (win_w, win_h) = (size.width as conrod::Scalar, size.height as conrod::Scalar);
+            if let Some(e) = conrod::backend::piston::event::convert(event.clone(), win_w, win_h) {
+                ui.handle_event(e);
+            }
+
+            event.update(|_| {
+                let mut ui = ui.set_widgets();
+                support::gui(&mut ui, &ids, &mut app);
+            });
+
+
+            window.draw_2d(&event, |context, graphics| {
+                if let Some(primitives) = ui.draw_if_changed() {
+
+                    // A function used for caching glyphs to the texture cache.
+                    let cache_queued_glyphs = |graphics: &mut G2d,
+                                               cache: &mut G2dTexture,
+                                               rect: conrod::text::rt::Rect<u32>,
+                                               data: &[u8]|
+                    {
+                        let offset = [rect.min.x, rect.min.y];
+                        let size = [rect.width(), rect.height()];
+                        let format = piston_window::texture::Format::Rgba8;
+                        let encoder = &mut graphics.encoder;
+                        text_vertex_data.clear();
+                        text_vertex_data.extend(data.iter().flat_map(|&b| vec![255, 255, 255, b]));
+                        UpdateTexture::update(cache, encoder, format, &text_vertex_data[..], offset, size)
+                            .expect("failed to update texture")
+                    };
+
+                    // Specify how to get the drawable texture from the image. In this case, the image
+                    // *is* the texture.
+                    fn texture_from_image<T>(img: &T) -> &T { img }
+
+                    // Draw the conrod `render::Primitives`.
+                    conrod::backend::piston::draw::primitives(primitives,
+                                                              context,
+                                                              graphics,
+                                                              &mut text_texture_cache,
+                                                              &mut glyph_cache,
+                                                              &image_map,
+                                                              cache_queued_glyphs,
+                                                              texture_from_image);
+                }
+            });
         }
     }
 }
