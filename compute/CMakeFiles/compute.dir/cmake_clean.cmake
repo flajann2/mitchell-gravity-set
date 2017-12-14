91bdcf0ef78978834924c6905f47b00f5b6bb2c3
Fred Mitchell <fred.mitchell@gmx.de>
2017-12-14 08:31:18 +0100

    ran the cmake and make, works.

---
+++ compute/CMakeFiles/compute.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compute.dir/compute.cpp.o"
+  "libcompute.pdb"
+  "libcompute.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compute.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
