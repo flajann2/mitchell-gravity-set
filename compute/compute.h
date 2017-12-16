abf58d6857119e1ba2c4a2eb910f06122b1598c8
Fred Mitchell <fred.mitchell@gmx.de>
2017-12-16 12:48:14 +0100

    added position, velocity, and acceleration classes, but need to flesh them out.

--- compute/compute.h
+++ compute/compute.h
@@ -1,15 +1,38 @@
 /*
  * MGS internal classes
  */
+
 #include <vector>
+#include <initializer_list>
+
 using namespace std;
 
-template <class T> struct Scalar {
+const int default_dimension= 2;
+
+template <class T = double> struct Scalar {
   T value;
   explicit Scalar(T v) : value(v) {}
 };
 
-template <class T, int D> struct Coords {
+template <class T = double> struct Coords {
   vector<T> vec;
-  //explicit Coords() {}
+  
+  Coords(int dim = default_dimension) { vec.resize(dim); }
+  Coords(initializer_list<T> list) : vec(list) {}
 };
+    
+template <class T = double> struct Position : public Coords<T> {
+    Position(int dim = default_dimension) : Coords<T>(dim)  {}
+    Position(initializer_list<T> list) : Coords<T>(list) {}  
+};
+
+ template <class T = double> struct Velocity : public Coords<T> {
+    Velocity(int dim = default_dimension) : Coords<T>(dim)  {}
+    Velocity(initializer_list<T> list) : Coords<T>(list) {}  
+};
+
+template <class T = double> struct Acceleration : public Coords<T> {
+    Acceleration(int dim = default_dimension) : Coords<T>(dim)  {}
+    Acceleration(initializer_list<T> list) : Coords<T>(list) {}  
+};
+
--- test/test.cpp
+++ test/test.cpp
@@ -3,15 +3,27 @@
 #include <string>
 #include <sstream>
 
+void pr_coord(auto prefix, auto c) {
+ for (int v : c.vec)
+   cout << prefix << ": " << v << endl;
+ cout << endl;
+}
+
 int main(int ac, char* av[]) {
-  Scalar<double> s {32.2};
-  Coords<double, 3> c;
-  string vstr(c.vec.begin(), c.vec.end());
+  Scalar<> s {32.2};
+  Coords<> c {3, 2.3, 22};
+  Coords<> cc;
+  Position<> p {0.01, 5.53, -34.1};
+  
   ostringstream cstr;
   cstr << s.value;
   
   std::cout << "Hello Test\n"
-            << cstr.str() << endl
-            << vstr << endl;
+            << cstr.str() << endl;
+            
 
+  pr_coord(" c", c);
+  pr_coord("cc", cc);
+  pr_coord(" p", p);
 }
+
