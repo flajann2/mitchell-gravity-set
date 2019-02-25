563d1dfcc52259b64a0c0947c980d75c99179400
Fred Mitchell <fred.mitchell@gmx.de>
2019-02-25 21:06:44 +0100

    :sparkles:we are creating pipelines

--- compute/marching_tetrahedra.h
+++ compute/marching_tetrahedra.h
@@ -3,8 +3,27 @@
 #include "compute.h"
 
 /**
+ * The Marching Tetrahedra is implemented here.
+ * We simply wish (for now) to walk the entire field
+ * and generate the triangles to render.
+ *
+ * TODO: later on we will optimise this to
+ * TODO: process in a more lazy fashion. For now,
+ * TODO: we will generate a "buffer" of polygons
+ * TODO: that will eventually be rendered.
  */
 
-namespace mgs {
+namespace mgs::march {
+  using namespace mgs;
 
+  /**
+   * Production is done in a pipeline fashion,
+   * and later will be made lazy. All classes
+   * involved in the pipeline shall be derived
+   * from this one.
+   */
+  class Pipeline {
+  };
+    
+  
 }
