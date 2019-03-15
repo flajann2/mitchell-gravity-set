d5b6c26c47b65187bb5dee844e05d4e450eb6010
Fred Mitchell <fred.mitchell@gmx.de>
2019-03-15 13:00:06 +0100

    :bug: now the tests compile

--- tests/test.cpp
+++ tests/test.cpp
@@ -96,7 +96,7 @@ TEST(ComputeTest, allOfIt) {
   // return RUN_ALL_TESTS();
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
