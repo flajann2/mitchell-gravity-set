4f1ed4769aab2d5cdd68aebca4b2c608e6bec881
Fred Mitchell <fred.mitchell@gmx.de>
2019-03-16 13:33:08 +0100

    :sparkles: in progress on the tetra

--- compute/marching_tetrahedra.cpp
+++ compute/marching_tetrahedra.cpp
@@ -1,4 +1,7 @@
 #include <marching_tetrahedra.h>
 
 namespace mgs::march {
+  void MakeTesselation::tesseltate_cube(const Index& idx){
+  }
+
 }
--- compute/marching_tetrahedra.h
+++ compute/marching_tetrahedra.h
@@ -16,6 +16,16 @@
 namespace mgs::march {
   using namespace mgs;
 
+  using tetra_index = std::array<unsigned char, 4>;
+  using cube_decomposer = std::array<tetra_index, 6>;
+
+  static const cube_decomposer dicer{{{0, 1, 3, 7},
+                                      {0, 2, 3, 7},
+                                      {0, 2, 6, 7},
+                                      {0, 4, 6, 7},
+                                      {0, 4, 5, 7},
+                                      {0, 1, 5, 7}}};
+
   /**
    * Production is done in a pipeline fashion,
    * and later will be made lazy. All classes
@@ -35,8 +45,14 @@ namespace mgs::march {
     MakeTesselation(const StarField& field) : m_field(field) {}
     MakeTesselation(const StarField&& field) : m_field(std::move(field)) {}
 
+    /**
+     * From the lower most point index, testelate
+     * the cube to the upper most point (basically by adding 1
+     * to the indices of the lmp).
+     */
+    void tesseltate_cube(const Index& lmp);
+
     template <typename Shape>
     Shape operator()() {}
   };
 }  // namespace mgs::march
-
