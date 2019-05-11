37313fdf7aad5915bd82a8dd6938d6d079f67284
Fred Mitchell <fred.mitchell@gmx.de>
2019-05-11 15:32:37 +0200

    Squashed 'submods/merseberg-incantation/' content from commit 84797dc
    git-subtree-dir: submods/merseberg-incantation
    git-subtree-split: 84797dc10d19e29fc516d6f96fbcc36f540373d8

---
+++ cmake_modules/AddGMock.cmake
@@ -0,0 +1,21 @@
+# Google Mock add (and fetch) macro
+macro(add_gmock)
+  set(ZIPNAME release-1.8.0)
+  set(ZIPFILE ${CMAKE_CURRENT_BINARY_DIR}/${ZIPNAME}.zip)
+  set(MOCKDIR ${CMAKE_CURRENT_BINARY_DIR}/googletest-${ZIPNAME})
+  
+  if(NOT IS_DIRECTORY ${MOCKDIR})
+    if(NOT EXISTS ${ZIPFILE})
+      message(STATUS "Downloading googletest...")
+      execute_process(COMMAND wget -nv -P ${CMAKE_CURRENT_BINARY_DIR} https://github.com/google/googletest/archive/${ZIPNAME}.zip)
+    endif()
+    message(STATUS "Extracting ${ZIPNAME}...")
+    execute_process(COMMAND unzip -q ${ZIPFILE} -d ${CMAKE_CURRENT_BINARY_DIR})
+  endif()
+  
+  if(IS_DIRECTORY ${MOCKDIR})
+    add_subdirectory(${MOCKDIR} ${CMAKE_CURRENT_BINARY_DIR}/gmock EXCLUDE_FROM_ALL)
+  else()
+    message(FATAL_ERROR "No ${MOCKDIR} found, can't add googletest")
+  endif()
+endmacro()
